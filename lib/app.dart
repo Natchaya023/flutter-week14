4e7c0306ceaeb07ac2fb47f9267860bd9b468a22
Natchaya023 <sakoleek93@gmail.com>
2022-08-19 16:19:17 +0700

    UP

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 import 'contact.dart';
 import 'home.dart';
+import 'list_view_builder.dart';
+import 'list_view_demo.dart';
+import 'list_view_menu.dart';
+import 'list_view_separate.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key); //1.constructor
@@ -10,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext buildContext) {
     //2.build
     return MaterialApp(
-      home: HomePage(),
+      home: ListViewMenu(),
     );
   }
 }
